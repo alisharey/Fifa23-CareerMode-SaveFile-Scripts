4b23f3a86d6ab5500d675e96cacdbf906b1584c4
ali abuzaid <ali.cairo2015@hotmail.com>
2022-10-01 20:35:31 +0800

    Removed unnecessary using statments.

--- FifaScripts/FileHandling.cs
+++ FifaScripts/FileHandling.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.IO;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using FifaLibrary;
+﻿using FifaLibrary;
 using System.Data;
-using System.Windows.Forms;
+
 
 namespace Fifa_Career_Script
 {
--- FifaScripts/Scripts.cs
+++ FifaScripts/Scripts.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+﻿using System.Data;
+
 
 namespace Fifa_Career_Script
 {
